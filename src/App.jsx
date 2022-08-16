889ec9df35ca8998b76a368be6f6920c78aaeb41
Sami <mohammadsamimsu@gmail.com>
2022-08-16 23:24:09 +0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import "./App.css";
+import Dashboard from './components/Dashboard2/Dashboard';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
